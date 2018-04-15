a01eb9ad15ca3f9db585165177cc060334a8977a
1601student <13643527180@193.com>
2018-04-15 19:48:34 +0800

    third commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,47 @@
 var gulp = require('gulp');
-var minsass = require('gulp-sass'); //把scss文件转换成css
+var sass = require('gulp-sass'); //把scss文件转换成css
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
-var htmljs = require('gulp-uglify');
-var htmlmin = require('gulp-htmlmin');
+var minjs = require('gulp-uglify');
+var minhtml = require('gulp-htmlmin');
 var url = require('url');
 var webserver = require('gulp-webserver');
+var sequence = require('gulp-sequence');
+var autoprefixer = require('gulp-autoprefixer');
+var mincss = require('gulp-clean-css');
+var clean = require('gulp-clean');
+
+gulp.task('clean', function() {
+    return gulp.src('dist')
+        .pipe(clean())
+})
+
+gulp.task('mincss', function() {
+    return gulp.src('src/css/*.scss')
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browers: ['last 2 versions', 'Android >= 4.0']
+        }))
+        .pipe(mincss())
+        .pipe(gulp.dest('dist/css'))
+})
+
+gulp.task('minjs', function() {
+    return gulp.src('src/js/*.js')
+        .pipe(minjs())
+        .pipe(gulp.dest('dist/js'))
+})
+
+var options = {
+    removeComments: true, //清除HTML注释
+    collapseWhitespace: true //压缩HTML
+}
+
+gulp.task('minhtml', function() {
+    return gulp.src('src/*.html')
+        .pipe(minhtml(options))
+        .pipe(gulp.dest('dist'))
+})
 
 gulp.task('webserver', function() {
     gulp.src('src')
@@ -13,7 +49,7 @@ gulp.task('webserver', function() {
             open: true,
             host: 'localhost',
             port: 8080,
-            livereload: false,
+            livereload: true,
             middleware: function(req, res, next) {
                 var pathname = url.parse(req.url, true).pathname;
                 if (pathname === '/newdata') {
@@ -49,4 +85,6 @@ gulp.task('webserver', function() {
         }))
 })
 
-gulp.task('default', ['webserver'])
+gulp.task('default', function(cb) {
+    sequence('clean', ['mincss', 'minjs', 'minhtml'], 'webserver', cb)
+})
